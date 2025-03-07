9fe053ffbe4c6e6493564cefd72ab9f24dd1f082
TurkiOmar77 <turkiomaralsaar@gmail.com>
2025-03-07 19:40:13 +0300

    end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Footer from "@/components/footer";
 import ListRooms from "@/components/listRooms";
 import Navbar from "@/components/Navbar";
 import SectionWelcome from "@/components/sectionWelcome";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, [router]);
+
   return (
     <>
-    <Navbar/>
-    <SectionWelcome/>
-    <ListRooms />
-    <Footer/>
+      <Navbar />
+      <SectionWelcome />
+      <ListRooms />
+      <Footer />
     </>
   );
 }
